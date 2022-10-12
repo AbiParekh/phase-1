a73587965082dc665d1033385b27db045fbdd399
S-OConnor <stephen.r.oconnor92@gmail.com>
2022-10-11 22:24:35 -0400

    FileIO Class and Method Definitions

---
+++ FileIO/fileIO.cpp
@@ -0,0 +1,20 @@
+#include "fileIO.h"
+
+bool FileIOManagement::writeVectorToFile(const std::string filePath, const std::string fileName, const std::vector<std::string>& items)
+{
+	//TODO implement
+	return true;
+}
+
+
+bool FileIOManagement::readFileIntoVector(const std::string filePath, const std::string fileName, std::vector<std::string>& items)
+{
+	//TODO implement
+	return true;
+}
+
+bool FileIOManagement::readNextLineFromFile(const std::string filePath, const std::string fileName, std::string& inputfile)
+{
+	//TODO Implement
+	return true;
+}
