25e9dbf4f86490aac9edc9934b7ecfbb0eb1d739
esevieux <esevieux@gmail.com>
2022-10-19 02:54:56 +0000

    merging Map class to mapReduceCMake dir

---
+++ mapReduceCMake/src/Map/TestMap.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _TestMap_h_
+#define _TestMap_h_
+
+#include "Map.h"
+#include "FileIO.h"
+
+#include <string>
+#include <regex>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+
+#endif
