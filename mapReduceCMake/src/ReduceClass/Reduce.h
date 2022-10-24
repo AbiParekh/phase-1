f0634c6e24e9b5afea7393426fc17be8d7db4d78
Abhishek Parekh <32465698+AbiParekh@users.noreply.github.com>
2022-10-24 18:59:57 -0400

    Update Reduce.h

--- mapReduceCMake/src/ReduceClass/Reduce.h
+++ mapReduceCMake/src/ReduceClass/Reduce.h
@@ -19,41 +19,54 @@ using std::ostream;
 
 
 typedef std::pair<string, int> vec;
-ostream& operator<<(ostream& output, const vec& v), int>>;
+ostream& operator<<(ostream& output, const vec& v);
 
 class Reduce{
     public:
 
             //variables, method titles, constructors and destructor
 
-            Reduce(); //constructor
+            Reduce(); 
+            //constructor
 
-            Reduce(const string intermediateDir); //with no memory allocation
+            Reduce(const string intermediateDir); 
+            //with no memory allocation
 
-            Reduce(const string intermediateDir, size_t buffer); //with buffer memory
+            Reduce(const string intermediateDir, size_t buffer); 
+            //with buffer memory
 
-            ~Reduce(); //destructor
+            ~Reduce(); 
+            //destructor
 
             bool importData(const string& filepath, const string& filename);
+            //import data from sorter class
+            //pass 
 
             std::vector<string> vec;
-            std::vector<std::pair<string, int>> reduced_vector;
+            //input vector with data inserted from the sorter
 
-            void reduce(const std::string& key, int& Iter start_val, int& Iter end_val, reduced_val); 
+            std::vector<std::pair<string, int>> reduced_vector;
+            //vector used for storing reduced values from the reduce function.
+
+
+            bool reduce(const std::string& key, int& val, int& reduced_val); 
             //four parameters, a string key, a start value, a end value, and a output redued value
             //Iterator will start from the front and iterate to the end for the values of a key
             //it will add to the counter how many it iterated through
             //key stays constant with no change
             //values will be added and put under the key it belongs to.
 
-            bool emptyBuffer(); //clean up memory
+            bool emptyBuffer(); 
+            //clean up memory
 
             std::vector<std::pair<string, int>>& exportReduce(); 
             //output vector with reduced values
 
-            bool exportReduce(const string filename, int val); //write to file and export
+            bool exportReduce(const string filename, const string key, const int val); 
+            //write to file and export
 
-            bool exportSuccess(const string filename); //write success to file and export
+            bool exportSuccess(const string filename); 
+            //write success to file and export
 
 
     private:
